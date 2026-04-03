f325c72b848079acc8eb28a604a879312ca495d2
OhSeungY <150667559+OhSeungY@users.noreply.github.com>
2026-04-03 09:42:44 +0900

    Update 1-3.c

---
+++ code/1-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void print_reverse(int *arr, int size){
+    arr += (size-1);
+    for(int i=0; i<size; i++){
+        if(i == size - 1){
+            printf("%d", *arr - i);
+        }else{
+            printf("%d ", *(arr - i));
+        }
+    }
+}
+
+int main() {
+    int nums[] = {1, 2, 3, 4, 5};
+
+    print_reverse(nums, 5);
+    printf("\n");
+
+    return 0;
+}
