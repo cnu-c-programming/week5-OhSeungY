4f3519f1e8b5065bdd92d4c57a1d3f0169b5e829
OhSeungY <150667559+OhSeungY@users.noreply.github.com>
2026-04-09 16:36:39 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void sort_strings(char **arr, int size){
+void sort_strings(char **arr, int size){ 
     char *temp;
     for(int i=0; i<size-1; i++){
         for(int j=0; j<size-1; j++){
